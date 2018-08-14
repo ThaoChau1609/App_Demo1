72642798e5040c81ca5c64f968df71b1d768c5c3
ThaoChau1609 <chauthuthao.iuh@gmail.com>
2018-08-14 13:34:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/student/app_demo/Screen_main.java
@@ -0,0 +1,13 @@
+package com.example.student.app_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_main);
+    }
+}
